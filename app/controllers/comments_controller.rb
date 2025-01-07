b4fc5b83e05df7820e58f7afa7e26973062c3a65
OddPixle <141264961+OddPixle@users.noreply.github.com>
2025-01-07 09:44:06 +0100

    docker stuff+extra

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
     before_action :set_post
     def create
         @post.comments.create!(params.expect(comment:[ :content]))
+        redirect_to @post
     end
     private
         def set_post
